01c0c1d593b03a90ca6c968087a7842c87842a12
Adrian Grupp <adrian.grupp@posteo.net>
2021-03-24 15:42:21 +0100

    started working on generic adaptive basis algo

--- src/adaptive_base.jl
+++ src/adaptive_base.jl
@@ -1,46 +1,89 @@
-# what method is used? -> hardcoded regression for now
-# if error increases 2 times in a row overfitting may occur and we have to exit or restart
+# TODO: if error increases 2 times in a row overfitting may occur and we have to exit or restart
+# TODO: multiple dispatch for bases on arbitrary weight functions
+# TODO: check for overfitting
 
 
+# TODO: what method is used? -> hardcoded regression for now
 
-function adaptiveBase(maxdeDree::Int = 10; method::String, m::Measure, model; Nrec::Int=maxDegree+1, ϵ = 1e-10)
+# Univariate adaptive basis algo
+# Arguments:
+#   * maxDegree: maximum degree searched for
+#   * op: polnomial basis of PCE model
+#   * model: function handle for model to evaluate
+#   * method: name of method to compute PCe coefficients
+# function adaptiveBasis(method::String, op::AbstractOrthoPoly, model; maxDegree::Int = 10, Nrec::Int = maxDegree+1, ϵ = 1e-10)
+function adaptiveBasis(PCEmodel, op; maxDegree::Int = 10, Nrec::Int = maxDegree+1, ϵ = 1e-10)
+    error = 1000 # error of current PC expansion
+    bestDegree = 1 # track best degree
     deg = 1
-    error = 1000
     
-
-    # TODO: check for overfitting
+    
     # Compare to preset threshold. Stop and return lowest error PCE, if condition is met.
     while error > ϵ && deg < maxDegree
         
         # 1. Compute current orthogonal basis
-        # TODO: based on provided measure
-        op = GaussOrthoPoly(deg; Nrec)
+        currentBasis = op(deg)
 
         # 2. Compute problem solution for current base
-        if method == "OLS"
+        # if method == "OLS"
             # sample measure
             # evaluate model, evaluate ortho polys
             # regression
-            PCE = leastSquares()
+            # PCE = leastSquares()
 
-        elseif method == "projection"
+        # elseif method == "projection"
             # evaluate model, evaluate ortho polys
             # integrate
-            PCE = ...
-        else
-            throw(error("method $method not implemented yet"))
-        end
+            # PCE = 1
+        # else
+            # throw(error("method $method not implemented yet"))
+        # end
+        PCE  = PCEmodel(deg)
+
 
         # 3. Compute generalization error for convergence
-        score = looError(Y, Φ, PCE)
+        error = looError(Y, Φ, PCE)
+        
+        # error = ComputeError
 
+        deg = deg + 1
+        
     end
+    
+    #TODO: save result as sparse array?
+    return PCE
 end
 
 
+function projectionModel(model::Function, op::AbstractOrthoPoly, maxDeg::Int)
+    t2 = Tensor(2, op)
 
+    coefficients = Array{Float64}(undef, maxDeg+1)
 
-function test()
-    op = GaussOrthoPoly()
+    for k = 0:maxDeg
+        # Function for evaluation of model and basis polynomial at given quadrature nodes t
+        g(t) = model(t) .* evaluate(k, t, op)
+        # Perfom numerical integration for the inner product <x, ϕ_k>
+        proj = integrate(g, op.quad)
+        # Inner product <ϕ_k,ϕ_k> for Galerkin projection
+        norm = t2.get([k,k])
+        # Coefficient is the fraction of the two inner products
+        coefficients[k+1] = proj / norm
+    end
 
+    return coefficients
 end
+
+
+# function test()
+    μ = 0
+    σ = 1
+    model(x) = exp.(μ + σ * x)
+    
+    # deg = 10
+    op(deg) = GaussOrthoPoly(deg, Nrec = deg * 2)
+
+    # projection(deg) = projectionModel(model, op(deg), deg)
+
+    # op_opti = adaptiveBasis(projection, op)
+# end
