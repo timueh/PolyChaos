54ae1d186481d7d0e8547e39fcb27cfbfe659de4
Adrian Grupp <adrian.grupp@posteo.net>
2021-05-10 17:10:37 +0200

    fix errors in leafe-one-out error estimation

--- src/error_estimation.jl
+++ src/error_estimation.jl
@@ -1,11 +1,5 @@
 import Statistics: var
 
-# Valiadation set
-    
-    # Not for now.
-
-    
-
 # Normalized empirical error (generalization error)
 # Following Blatman2009 (Dissertation)
 function empError(Y::Vector{Float64}, Φ::Matrix{Float64}, pceCoeffs; adjusted = false)
@@ -25,7 +19,7 @@ function empError(Y::Vector{Float64}, Φ::Matrix{Float64}, pceCoeffs; adjusted =
     empError = 1/n * sum( (Y[i] - Y_Pce[i])^2 for i in 1:n )
     
     # Normalize with variance
-    nempError = varY == 0 ? 0.0 : empError / varY
+    nempError = varY == 0 ? 0 : empError / varY
 
 
     if adjusted
@@ -39,7 +33,6 @@ end
 
 
 
-
 # Leave-one-out cross-validation error
 function looError(Y::Vector{Float64}, Φ::Matrix{Float64}, pceCoeffs)
     @assert length(Y) > 0           "Empty results vector Y"
@@ -49,22 +42,30 @@ function looError(Y::Vector{Float64}, Φ::Matrix{Float64}, pceCoeffs)
     Y_Pce = Φ * pceCoeffs
 
     # h-factor for validation sets
+    # println(Φ * Φ')
+    # println(inv(Φ' * Φ))
+    # println(Φ * inv(Φ' * Φ))
     M = Φ * inv(Φ' * Φ) * Φ' #TODO: fix conditioning
     N = size(M, 1)
     h = ones(N) - diag(M)
-    
-    looError = 1/N * sum( ( (Y[i] - Y_Pce[i]) / h[i] )^2 for i in 1:N )
     
     # Variance of sampling evaluation (true model)
     n = length(Y)
     meanY = mean(Y)
     varY = n > 1 ? 1/(n-1) * sum( (Y[i] - meanY)^2 for i in 1:n ) : 0
+    
+    # compute squared error with h-factor
+    loo = 1/N * sum( ( (Y[i] - Y_Pce[i]) / h[i] )^2 for i in 1:N )
+    
+    # println("M: ", M)
+    # println("Y - Y_Pce: ", Y - Y_Pce)
+    # println("h: ", h)
+    # println("varY: ", varY)
+    # println("looError: ", loo)
 
-    println("M: ", M)
-    println("h: ", h)
-    println("varY: ", varY)
-    println("looError: ", looError)
+    # Normalize error with variance. Set error to 0, if var is 0
+    looError = varY == 0 ? 0 : loo / varY
 
-    # Return error normalize with variance
-    varY == 0 ? 0.0 : looError / varY
+    # Return loo error. Return Inf, if it is NaN. Can happen for underdetermined problems.
+    return isnan(looError) ? Inf : looError
 end
