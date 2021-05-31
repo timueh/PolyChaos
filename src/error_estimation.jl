320e20b41194817a43ca22a6ca147f389c43fa81
Adrian Grupp <adrian.grupp@posteo.net>
2021-05-31 15:36:01 +0200

    small code refactoring and docu

--- src/error_estimation.jl
+++ src/error_estimation.jl
@@ -1,64 +1,60 @@
 export empError, 
        looError
 
-# Normalized empirical error (generalization error)
-# Following Blatman2009 (Dissertation)
+# Normalized empirical error / generalization error (Blatman2009 - Dissertation)
+# nemError ∈ [0,1]
 function empError(Y::Vector{Float64}, Φ::Matrix{Float64}, pceCoeffs; adjusted = false)
     @assert length(Y) > 0           "Empty results vector Y"
-    @assert length(Y) == size(Φ, 1) "Inconsistent number of elements."
+    @assert length(Y) == size(Φ, 1) "Inconsistent number of elements"
     # TODO: Require column vectors
 
     # Compute PCE model response
     Y_Pce = Φ * pceCoeffs
     
-    # Variance of sampling evaluation (true model)
+    # Empirical variance of sampling evaluation (true model)
     n = length(Y)
-    meanY = mean(Y)
-    varY = n > 1 ? 1/(n-1) * sum( (Y[i] - meanY)^2 for i in 1:n ) : 0
+    varY = n > 1 ? 1/(n-1) * sum( (Y[i] - mean(Y))^2 for i in 1:n ) : 0
 
     # Empirical error
     empError = 1/n * sum( (Y[i] - Y_Pce[i])^2 for i in 1:n )
     
-    # Normalize with variance
-    nempError = varY == 0 ? 0 : empError / varY
-
-
     if adjusted
         # TODO: adjusted empirical error (penalize larger PCE bases)
         # p = ... #bincoeff
-        # empError2 = (n-1) / (n - p - 1) * ( empError)
+        # n = ...
+        # empError = (n-1) / (n - p - 1) * ( empError)
     end
+
+    # Normalize with variance
+    nempError = varY == 0 ? 0 : empError / varY
 
     return nempError
 end
 
 
 
-# Leave-one-out cross-validation error
-# Following Blatman2009 (Dissertation)
+# Leave-one-out cross-validation error (Blatman2009 - Dissertation)
+# Note: looError ∈ [0,∞]. If > 1, average modeling would be better.
 function looError(Y::Vector{Float64}, Φ::Matrix{Float64}, pceCoeffs)
     @assert length(Y) > 0           "Empty results vector Y"
-    @assert length(Y) == size(Φ, 1) "Inconsistent number of elements."
+    @assert length(Y) == size(Φ, 1) "Inconsistent number of elements"
 
     # Compute PCE model response
     Y_Pce = Φ * pceCoeffs
 
     # h-factor for validation sets
-    # println(Φ * Φ')
-    # println(inv(Φ' * Φ))
-    # println(Φ * inv(Φ' * Φ))
     M = Φ * inv(Φ' * Φ) * Φ' #TODO: fix conditioning
     N = size(M, 1)
     h = ones(N) - diag(M)
     
-    # Variance of sampling evaluation (true model)
+    # Empirical variance of sampling evaluation (true model)
     n = length(Y)
-    meanY = mean(Y)
-    varY = n > 1 ? 1/(n-1) * sum( (Y[i] - meanY)^2 for i in 1:n ) : 0
+    varY = n > 1 ? 1/(n-1) * sum( (Y[i] - mean(Y))^2 for i in 1:n ) : 0
     
     # compute squared error with h-factor
     loo = 1/N * sum( ( (Y[i] - Y_Pce[i]) / h[i] )^2 for i in 1:N )
     
+    ## Debuging
     # println("M: ", M)
     # println("Y - Y_Pce: ", Y - Y_Pce)
     # println("h: ", h)
